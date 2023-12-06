c4fdd5718d9023cd8904004b4a16741d02de9bd4
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-07 02:41:57 +0900

    Moved all Modal view logic into Modal component

--- frontend/src/lib/Modal.svelte
+++ frontend/src/lib/Modal.svelte
@@ -1,19 +1,41 @@
 <script>
-  export let mouseX, mouseY = 0
+  import { slide } from 'svelte/transition'
 
   let modalVisible = false
+  let mouseX, mouseY = 0
+
+  function toggleModal(e) {
+    if (modalVisible) {
+      modalVisible = false
+    } else {
+      const maxX = window.innerWidth - 100 //Arbitrary placeholder numbers
+      const maxY = window.innerHeight - 60 //Arbitrary placeholder numbers
+      mouseX = Math.min(e.clientX, maxX)
+      mouseY = Math.min(e.clientY, maxY)
+      modalVisible = true
+    }
   }
 </script>
 
-{#if $modalVisible}
-  <div class='modal' style="left: {mouseX}px; top: {mouseY}px" on:click|stopPropagation={hideModal}>
-  {mouseX}, {mouseY}
-  </div>
-{/if}
+<div class="modalWrapper" on:click={toggleModal}>
+  {#if modalVisible}
+    <div class='modal' on:click|stopPropagation transition:slide|local style="left: {mouseX}px; top: {mouseY}px">
+      {mouseX}, {mouseY}
+    </div>
+  {/if}
+</div>
 
 
 <style>
-  div {
+  .modalWrapper {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: #F00;
+  }
+  .modal {
     position: absolute;
     border: 1px solid black;
     padding: 16px;
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,32 +1,13 @@
 <script>
   import Modal from '/src/lib/Modal.svelte'
-  let mouseX, mouseY = 0
-
-  function showModal(e) {
-    const maxX = window.innerWidth - 100 //Arbitrary placeholder numbers
-    const maxY = window.innerHeight - 60 //Arbitrary placeholder numbers
-    mouseX = Math.min(e.clientX, maxX)
-    mouseY = Math.min(e.clientY, maxY)
-    modalVisible.set(true)
-  }
-
 </script>
 
-<div class="page" on:click={showModal}>
-
-  <Modal mouseX={mouseX} mouseY={mouseY}/>
-
-</div>
+<Modal />
   
 <style>
   :global(body, html) {
     height: 100%;
     margin: 0;
     padding: 0;
-  }
-  .page {
-    height: 100%;
-    width: 100%;
-    background-color: #F00;
   }
 </style>
