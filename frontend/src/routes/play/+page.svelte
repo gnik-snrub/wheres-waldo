1e8a2ca2f67116bfa3827525b05383e150cbe05c
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-08 04:27:17 +0900

    Added images to play route

--- frontend/src/routes/play/+page.svelte
+++ frontend/src/routes/play/+page.svelte
@@ -1,4 +1,19 @@
 <script>
   import Modal from '/src/lib/Modal.svelte'
+  import grey_knights from '/src/images/greyknights.jpeg'
+  import marinevsnids from '/src/images/marinevsnids.jfif'
+  import marinevsnids2 from '/src/images/marinevsnids2.jfif'
 </script>
+
 <Modal />
+
+  <img src={grey_knights} />
+  <img src={marinevsnids} />
+  <img src={marinevsnids2} />
+
+<style>
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
