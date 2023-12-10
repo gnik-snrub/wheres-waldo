89f6f818befa36830982c374866de148f86bad52
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-11 03:40:08 +0900

    Changed /play route to use trigger toggleModal child function

--- frontend/src/routes/play/+page.svelte
+++ frontend/src/routes/play/+page.svelte
@@ -3,14 +3,20 @@
   import grey_knights from '/src/images/greyknights.jpeg'
   import marinevsnids from '/src/images/marinevsnids.jfif'
   import marinevsnids2 from '/src/images/marinevsnids2.jfif'
+
+  let makeModal
+
+function onImageClick(e) {
+  makeModal(e)
+}
 </script>
 
-<Modal />
 <a href="/">Home</a>
 
-<img src={grey_knights} />
-<img src={marinevsnids} />
-<img src={marinevsnids2} />
+<img src={marinevsnids} on:click={onImageClick} />
+<img src={grey_knights} on:click={onImageClick} />
+<img src={marinevsnids2} on:click={onImageClick} />
+<Modal bind:toggleModal={makeModal} />
 
 <style>
   img {
