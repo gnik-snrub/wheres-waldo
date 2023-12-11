8ad60feaa72e77c15d6f150ef781d5c1108cac82
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-12 05:34:19 +0900

    Added DotGothic16 font and changed font colors

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,10 +4,13 @@
 <slot />
   
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=DotGothic16');
   :global(body, html) {
     height: 100vh;
     margin: 0;
     padding: 0;
+    font-family: 'DotGothic16';
+    color: #ccc;
   }
   header {
     max-height: 10%;
@@ -17,6 +20,11 @@
     text-align: center;
     margin: 0;
     padding-top: 0.7em;
-    font-family:Arial, Helvetica, sans-serif;
+  :global(a, a:visited, a:active) {
+    color: #ccc;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #fff;
   }
 </style>
