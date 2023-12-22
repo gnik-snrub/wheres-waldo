edf9350fb726019705f2bc1b69565c2beefd1d64
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-23 01:45:15 +0900

    Styled /leaderboard route

--- frontend/src/routes/leaderboard/+page.svelte
+++ frontend/src/routes/leaderboard/+page.svelte
@@ -14,3 +14,45 @@
   </ol>
   <a href="/">Back</a>
 </section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding-top: 6em;
+    height: 100%;
+    width: 100%;
+  }
+  ol {
+    display: flex;
+    flex-direction: column;
+    width: min(500px, 80%);
+    margin: 0;
+    padding: 0;
+    justify-content: center;
+    margin-bottom: 2em;
+  }
+  li {
+    display: grid;
+    grid-template-columns: 10% 70% 20%;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    padding: 0.5em 0;
+  }
+  li *:first-child {
+    text-align: center;
+  }
+  li *:nth-child(2) {
+    margin-right: 1em;
+    padding-left: 1em;
+    border-left: 1px solid #ccc;
+  }
+  li *:last-child {
+    text-align: center;
+    border-left: 1px solid #ccc;
+  }
+  span {
+    overflow-wrap: anywhere;
+  }
+</style>
