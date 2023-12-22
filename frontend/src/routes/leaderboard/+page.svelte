d7657867b1a6e35f8a34d40cfcaf28e3a1c82c9a
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-23 01:46:27 +0900

    Added blur in transition to leaderboard

--- frontend/src/routes/leaderboard/+page.svelte
+++ frontend/src/routes/leaderboard/+page.svelte
@@ -1,18 +1,29 @@
+<script>
+  import { blur } from 'svelte/transition'
+  import { onMount } from 'svelte';
+  let mounted = false
+  onMount(() => {
+    mounted = true
+  })
+</script>
+
 <section>
   <h2>Leaderboard</h2>
-  <ol>
-    <li>
-      <h3>Name</h3>
-      <h3>Time</h3>
-    </li>
-    {#each listData as {name, time} }
+  {#if mounted}
+    <ol transition:blur>
       <li>
-        <span>{name}</span>
-        <span>{time} sec</span>
+        <h3>Name</h3>
+        <h3>Time</h3>
       </li>
-    {/each}
-  </ol>
-  <a href="/">Back</a>
+      {#each listData as {name, time} }
+        <li>
+          <span>{name}</span>
+          <span>{time} sec</span>
+        </li>
+      {/each}
+    </ol>
+    <a href="/">Back</a>
+  {/if}
 </section>
 
 <style>
