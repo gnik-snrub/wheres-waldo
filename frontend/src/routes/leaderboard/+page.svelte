54cb5db2ce89cea83fca35bf66174130e8fee671
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-23 01:43:28 +0900

    Added semantic markup of /leaderboard route

--- frontend/src/routes/leaderboard/+page.svelte
+++ frontend/src/routes/leaderboard/+page.svelte
@@ -1,3 +1,16 @@
 <section>
-  <a href="/">Home</a>
+  <h2>Leaderboard</h2>
+  <ol>
+    <li>
+      <h3>Name</h3>
+      <h3>Time</h3>
+    </li>
+    {#each listData as {name, time} }
+      <li>
+        <span>{name}</span>
+        <span>{time} sec</span>
+      </li>
+    {/each}
+  </ol>
+  <a href="/">Back</a>
 </section>
