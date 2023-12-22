b628a90ee0844cd446ed46428d8d3ac9fbb99585
Josiah Morris <josiahbmorris@hotmail.com>
2023-12-23 01:48:16 +0900

    Added counter and increment() to track rankings

--- frontend/src/routes/leaderboard/+page.svelte
+++ frontend/src/routes/leaderboard/+page.svelte
@@ -5,6 +5,12 @@
   onMount(() => {
     mounted = true
   })
+
+  let counter = 0
+  function increment() {
+    counter++
+    return counter
+  }
 </script>
 
 <section>
@@ -12,11 +18,13 @@
   {#if mounted}
     <ol transition:blur>
       <li>
+        <h3>#</h3>
         <h3>Name</h3>
         <h3>Time</h3>
       </li>
       {#each listData as {name, time} }
         <li>
+          <span>#{increment()}</span>
           <span>{name}</span>
           <span>{time} sec</span>
         </li>
