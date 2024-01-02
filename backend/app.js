da7b60c85c1992ff89cbb3c997f39d3afc0efe72
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-03 05:24:03 +0900

    Added /api route to backend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+const apiRouter = require('./routes/api')
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,5 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', apiRouter)
 
 module.exports = app;
