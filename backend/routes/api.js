858cf7371e5de838e97c973e9649e879c1517600
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-03 05:41:42 +0900

    Added API endpoints

---
+++ backend/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const apiController = require('../controllers/apiController')
+
+router.get('/startGame', apiController.startGame)
+
+router.get('/checkSuccess', apiController.checkSuccess)
+
+router.post('/addToLeaderboard', apiController.pushToLeaderboard)
+router.get('/leaderboardData', apiController.getLeaderboardData)
