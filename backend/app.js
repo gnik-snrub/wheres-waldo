4e82956c15752623d2f741c936c524445b96e4dd
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-04 02:43:31 +0900

    Set backend to listen on .env.PORT

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const apiRouter = require('./routes/api')
 
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -12,5 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter)
+
+app.listen(process.env.PORT, () => console.log('Server started on port ' + process.env.PORT))
 
 module.exports = app;
