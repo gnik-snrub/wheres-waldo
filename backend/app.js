3bed92a00b4a71cd3e0efc9fa8d476c0eaf70fa1
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-04 02:43:40 +0900

    Setup cors on backend

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const cors = require('cors')
+app.use(cors())
+
 app.use('/api', apiRouter)
 
 app.listen(process.env.PORT, () => console.log('Server started on port ' + process.env.PORT))
