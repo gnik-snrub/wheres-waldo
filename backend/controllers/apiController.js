3235e0d901d4680490b91a9993321b6c18ae21e3
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-04 02:38:53 +0900

    Created checkSuccess endpoint, which returns a time, alongside the requested data

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -2,3 +2,19 @@ exports.startGame = async(req, res) => {
   res.status(200).json({startTime: new Date().getTime()})
 }
 
+exports.checkSuccess = async(req, res) => {
+  const successLocations = [
+    {image: 1, x1: 23, x2: 29, y1: 25, y2: 35},
+    {image: 1, x1: 76, x2: 79, y1: 63, y2: 74},
+    {image: 2, x1: 19, x2: 36, y1: 38, y2: 56},
+    {image: 2, x1: 64, x2: 76, y1: 32, y2: 48},
+    {image: 3, x1: 36, x2: 41, y1: 23, y2: 38},
+    {image: 3, x1: 76, x2: 88, y1: 64, y2: 78}
+  ]
+  res.status(200)
+    .json({
+      endTime: new Date().getTime(),
+      ...successLocations[req.body.selectedImageNumber]
+    })
+}
+
